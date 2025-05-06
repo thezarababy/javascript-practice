7106631e0d764353bb2f4c2a0b8c95b6f3ca67d4
thezarababy <jyokoduwa@gmail.com>
2025-05-06 11:03:50 +0100

    Operators

--- script.js
+++ script.js
@@ -24,3 +24,31 @@ const sentence= `hello ${firstName}  ${lastName}! How are you?`;
  }
  console.log(myData)
  console.log(typeof myData)
+ //Array
+  const myCars = ["Benz","BMW","Suzuki","Lexus", "Jeep","Toyota"];
+//   console.log(typeof myCars);
+  console.log(myCars[3]);
+
+  // Arithmetic Operators
+  const x = 2;
+  const y = 4;
+//   const sum = x + y;
+//const times = x * y;
+// const  divide = y/x;
+const remainder = 2%4;
+
+  console.log(remainder)
+  
+  //Assignment Operators
+  let z = 19;
+//   +=
+//   -+
+  //Comparism Operators
+   let r = "Joy";
+   let s = "oge"
+   let t = 34;
+   let u = "Joy";
+   let v = 54;
+   console.log(r==u);
+   console.log(s==r);
+   
