7764cee0c7dd6aaa4ea831fd3ca0dc82cd23bc2c
thezarababy <jyokoduwa@gmail.com>
2025-05-06 10:24:39 +0100

    object

---
+++ script.js
@@ -0,0 +1,26 @@
+const monthlyRent=500;
+const yearlyRent=monthlyRent * 12;
+const decadelyRent=monthlyRent * 10;
+console.log(yearlyRent);
+console.log(decadelyRent);
+
+const myName="joy"
+
+console.log(myName);
+ const firstName= "joy";
+ const lastName = "okoduwa"
+//  const sentence ="Hello " + firstName + " " + lastName + "! How are you?!";
+const sentence= `hello ${firstName}  ${lastName}! How are you?`;
+ console.log(sentence)
+  const myCountry = "Nigeria";
+  console.log(myCountry)
+//    myCountry ="Italy";
+//object
+
+ const myData = {
+    name:"joy",
+    sex: "female",
+    Age:"25",
+ }
+ console.log(myData)
+ console.log(typeof myData)
